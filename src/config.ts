e5ed8f11a559eb85d82c6305fb7a5f221180f776
john <muxitsy@gmail.com>
2025-05-14 20:23:11 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,6 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		LinkPreset.FriendLink,
 		{
 			name: "GitHub",
 			url: "https://github.com/chenyueergui", // Internal links should not include the base path, as it is automatically added
