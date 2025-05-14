c04ecead36af9521bded220a8511bfdcad7194dd
JohnFoxen <muxitsy@gmail.com>
2025-05-14 18:42:12 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,6 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://chenyueergui.github.io/",
-	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
